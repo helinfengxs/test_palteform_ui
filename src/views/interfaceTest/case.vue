0d52fcc4e6ff3875b7d0fc9ac34f33e3a601f6fb
helinfeng <helinfengxs@163.com>
2021-05-16 23:16:38 +0800

    接口用例页面

--- src/views/interfaceTest/case.vue
+++ src/views/interfaceTest/case.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="app-container">
-    接口用例
+    <template>
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          label="日期"
+          width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+      <div style="margin-top: 20px">
+        <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
+        <el-button @click="toggleSelection()">取消选择</el-button>
+      </div>
+    </template>
   </div>
 </template>
 <script>
